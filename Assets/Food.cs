896383d0b5ce3f55df83bec24ca2f12e721fe06f
Akash <akash.katailiha120@gmail.com>
2024-11-21 02:39:52 +0530

    Add Snake and Food Functionality

---
+++ Assets/Food.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class Food : MonoBehaviour
+{
+    public Collider2D spawnArea;
+    private Snake snake;
+    public bool massBurner;
+    private void Awake()
+    {
+        snake = FindObjectOfType<Snake>();
+    }
+
+    private void Start()
+    {
+        RandomizePosition();
+    }
+
+    public void RandomizePosition()
+    {
+        Bounds bounds = spawnArea.bounds;
+
+        // Pick a random position inside the bounds
+        // Round the values to ensure it aligns with the grid
+        int x = Mathf.RoundToInt(Random.Range(bounds.min.x, bounds.max.x));
+        int y = Mathf.RoundToInt(Random.Range(bounds.min.y, bounds.max.y));
+
+        // Prevent the food from spawning on the snake
+        while (snake.Occupies(x, y))
+        {
+            x++;
+
+            if (x > bounds.max.x)
+            {
+                x = Mathf.RoundToInt(bounds.min.x);
+                y++;
+
+                if (y > bounds.max.y)
+                {
+                    y = Mathf.RoundToInt(bounds.min.y);
+                }
+            }
+        }
+
+        transform.position = new Vector2(x, y);
+        int randomTime = Random.Range(4, 8);
+        Invoke("RandomizePosition", randomTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        CancelInvoke("RandomizePosition");
+        RandomizePosition();
+    }
+}
---
+++ Assets/Snake.cs
@@ -0,0 +1,160 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Snake : MonoBehaviour
+{
+    public Transform snakePart;
+    public Vector2Int direction = Vector2Int.right;
+    public float speed = 20f;
+    public float speedMultiplier = 1f;
+    public int initialSize = 4;
+    public bool moveThroughWalls = false;
+
+    private List<Transform> segments = new List<Transform>();
+    private float nextUpdate;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ResetState();
+    }
+
+    private void Update()
+    {
+        ChangeDirection();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Time.time < nextUpdate)
+        {
+            return;
+        }
+
+        for (int i = segments.Count - 1; i > 0; i--)
+        {
+            segments[i].position = segments[i - 1].position;
+        }
+
+
+        int x = Mathf.RoundToInt(transform.position.x) + direction.x;
+        int y = Mathf.RoundToInt(transform.position.y) + direction.y;
+        transform.position = new Vector2(x, y);
+
+        nextUpdate = Time.time + (1f / (speed * speedMultiplier));
+    }
+
+    public void Grow()
+    {
+        Transform segment = Instantiate(snakePart);
+        segment.position = segments[segments.Count - 1].position;
+        segments.Add(segment);
+    }
+
+    public void ResetState()
+    {
+        direction = Vector2Int.right;
+        transform.position = Vector3.zero;
+
+        // Start at 1 to skip destroying the head
+        for (int i = 1; i < segments.Count; i++)
+        {
+            Destroy(segments[i].gameObject);
+        }
+
+        // Clear the list but add back this as the head
+        segments.Clear();
+        segments.Add(transform);
+
+        // -1 since the head is already in the list
+        for (int i = 0; i < initialSize - 1; i++)
+        {
+            Grow();
+        }
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Food"))
+        {
+            Grow();
+        }
+        else if (other.gameObject.CompareTag("Obstacle"))
+        {
+            ResetState();
+        }
+        else if (other.gameObject.CompareTag("Wall"))
+        {
+            if (moveThroughWalls)
+            {
+                Traverse(other.transform);
+            }
+            else
+            {
+                ResetState();
+            }
+        }
+    }
+
+    public bool Occupies(int x, int y)
+    {
+        foreach (Transform segment in segments)
+        {
+            if (Mathf.RoundToInt(segment.position.x) == x &&
+                Mathf.RoundToInt(segment.position.y) == y)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+
+    void ChangeDirection()
+    {
+        if (direction.x != 0f)
+        {
+            if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                direction = Vector2Int.up;
+            }
+            else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                direction = Vector2Int.down;
+            }
+        }
+        else if (direction.y != 0f)
+        {
+            if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                direction = Vector2Int.right;
+            }
+            else if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                direction = Vector2Int.left;
+            }
+        }
+    }
+
+    void Traverse(Transform wall)
+    {
+
+        if (direction.x != 0)
+        {
+            transform.position = new Vector2(-wall.position.x + direction.x, transform.position.y);
+        }
+        else
+        if (direction.y != 0)
+        {
+            transform.position = new Vector2(transform.position.x, -wall.position.y + direction.y);
+        }
+
+    }
+
+
+
+}
